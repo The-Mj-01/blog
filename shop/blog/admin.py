050fb06b6963510ebfaef5bd061028913601b1bc
Mj <the.king.mj99@gmail.com>
2019-08-10 13:52:04 +0430

    Initial commit 2

--- shop/blog/admin.py
+++ shop/blog/admin.py
@@ -1,4 +1,48 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.core import serializers
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+
+def export_as_json(modeladmin , request , queryset):
+    response = HttpResponse(content_type = "application/json")
+
+    serializers.serialize("json" , queryset , stream= response)
+
+    return response
+
+def make_published(modeladmin , request , queryset):
+    result = queryset.update(status = 'published')
+    if result == 1:
+        message_bit = "1 post was"
+    else:
+        message_bit = "{} post were".format(result)
+    modeladmin.message_user(request , "{} successfully marked as published.".format(message_bit))
+
+
+def make_draft(modeladmin , request , queryset):
+    result = queryset.update(status = 'draft')
+    if result == 1:
+        message_bit = "1 post was"
+    else:
+        message_bit = "{} post were".format(result)
+    modeladmin.message_user(request , "{} successfully marked as draft.".format(message_bit))
+
+
+
+
+make_published.short_description = 'Mark selected posts as published'
+make_draft.short_description = 'Mark selected posts as draft'
+export_as_json.short_description = 'Export selected posts as json request'
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title' , 'slug' , 'publish' , 'status')
+    list_filter = ('publish' , 'status')
+    search_fields = ('title' , 'body')
+    ordering = ['status' , 'publish']
+    prepopulated_fields = {'slug': ('title',)}
+    actions = [make_published , make_draft , export_as_json]
+
+# admin.site.register(Post , PostAdmin)
--- shop/blog/models.py
+++ shop/blog/models.py
@@ -6,16 +6,20 @@ from django.utils import timezone
 
 class Post (models.Model):
     STATUS_CHOICES = (
-        ('draft' , 'Draft'),
-        ('published' , 'Published'),
+        ('draft' , 'آماده انتشار'),
+        ('published' , 'منتشر شده'),
     )
-    title = models.CharField(max_length=60)
-    slug = models.SlugField(max_length=100)
-    body = models.TextField()
-    publish = models.DateTimeField(default = timezone.now)
-    created = models.DateTimeField(auto_now_add = True)
-    updated = models.DateTimeField(auto_now = True)
-    status = models.CharField(max_length=60 , choices=STATUS_CHOICES , default= 'draft')
+    title = models.CharField(max_length=60 , verbose_name='عنوان')
+    slug = models.SlugField(max_length=100 , verbose_name='لینک')
+    body = models.TextField(verbose_name='متن کامل')
+    publish = models.DateTimeField(default = timezone.now , verbose_name='تاریخ انتشار')
+    created = models.DateTimeField(auto_now_add = True , verbose_name='تاریخ ثبت')
+    updated = models.DateTimeField(auto_now = True , verbose_name='تاریخ ویرایش')
+    status = models.CharField(max_length=60 , choices=STATUS_CHOICES , default= 'draft', verbose_name='وضعیت')
 
+
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
     def __str__(self):
         return "Post object( id = {} & title = {} )".format(self.id , self.title)
